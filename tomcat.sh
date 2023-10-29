69deacb02fdb17028330fdb98620edd56aa9abae
MudassirKhan22 <khanmudassir2000@gmail.com>
2023-10-29 18:52:42 +0530

    Adding tomcat server config file

--- tomcat.sh
+++ tomcat.sh
@@ -4,8 +4,9 @@ sudo yum install tomcat -y
 NEW_PORT="8081" 
 SERVER_XML="/usr/share/tomcat/conf/server.xml"
 
-if grep -q 'Connector port="8080"' "$SERVER_XML"; then
-    sed -i "s/Connector port=\"8080\"/Connector port=\"$NEW_PORT\"/" "$SERVER_XML"
+if grep -q 'Connector port="[0-9]*"' "$SERVER_XML"; 
+then
+    sed -i "s/Connector port=\"[0-9]*\"/Connector port=\"$NEW_PORT\"/" "$SERVER_XML"
     echo "Tomcat HTTP port set to $NEW_PORT"
 else
     echo "Tomcat HTTP port already set to $NEW_PORT"
