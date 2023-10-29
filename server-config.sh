d8e9ce531a024854e32f9b406aaa8c615c4e16ae
MudassirKhan22 <khanmudassir2000@gmail.com>
2023-10-29 13:00:10 +0530

    Updates

--- server-config.sh
+++ server-config.sh
@@ -2,7 +2,7 @@
 sudo yum install git -y
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install maven install -y
-if[ -d "New-addressbook" ]
+if [ -d "New-addressbook" ]
 then
     echo "Repository is already cloned  and exist"
     cd /home/ec2-user/New-addressbook
@@ -12,4 +12,4 @@ else
 fi
     cd /home/ec2-user/New-addressbook
     mvn package
-    
+
